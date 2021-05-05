828b79260537d8ab7a16a79c36b66ad2bdc85100
yolo <77111763+yoloeww@users.noreply.github.com>
2021-05-05 21:46:32 +0800

    Add files via upload

---
+++ 2021.5.5.数组同时求最大最小值.cpp
@@ -0,0 +1,14 @@
+# include<stdio.h>
+int main(void)
+{
+	int x[]={3,5,2,98,214,523},max,min;
+	int *p=x;
+	min=max=x[0];
+	for(;p<=x+5;p++)
+	{
+	   if(max<*p) max=*p;
+	   if(min>*p) min=*p;
+	}
+	printf("max=%d,min=%d\n",max,min);
+	return 0;
+}
