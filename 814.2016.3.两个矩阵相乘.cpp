9bd667ddbdd88dfa5c55952c361690b3936d3eff
yolo <77111763+yoloeww@users.noreply.github.com>
2021-07-19 16:52:54 +0800

    Rename 2016.3.两个矩阵相乘.cpp to 814.2016.3.两个矩阵相乘.cpp

--- 814.2016.3.两个矩阵相乘.cpp
+++ 814.2016.3.两个矩阵相乘.cpp
@@ -6,19 +6,19 @@
 int main(void)
 {  
 	int a[100][100],b[100][100],c[100][100]={0},i,j;
-	printf("ÇëÊäÈëÒ»¸öxĞĞyÁĞµÄA¾ØÕó:\n");
+	printf("è¯·è¾“å…¥ä¸€ä¸ªxè¡Œyåˆ—çš„AçŸ©é˜µ:\n");
     for(i=0;i<x;i++)
 	  {
 		  for(j=0;j<y;j++)
                  scanf("%d",&a[i][j]);
 	  }
-    printf("ÇëÊäÈëÒ»¸öyĞĞzÁĞµÄB¾ØÕó:\n");
+    printf("è¯·è¾“å…¥ä¸€ä¸ªyè¡Œzåˆ—çš„BçŸ©é˜µ:\n");
      for(i=0;i<y;i++)
 	  {
 		  for(j=0;j<z;j++)
                  scanf("%d",&b[i][j]);
 	  }
-    printf("A*BµÄĞÂ¾ØÕóÎª:\n");
+    printf("A*Bçš„æ–°çŸ©é˜µä¸º:\n");
 	for(i=0;i<x;i++)
 	{
 		  for(j=0;j<z;j++)
